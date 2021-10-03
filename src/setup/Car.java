42b23aee9b840bce21eeeb8f00ec94c0ce50555c
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-03 15:35:20 -0400

    merge

--- src/setup/Car.java
+++ src/setup/Car.java
@@ -2,9 +2,20 @@ package setup;
 
 public class Car {
 
+    public static void main(String[] args) {
+
+    }
+
     // this is car class from master branch
 
     // we will have conflict
 
     //because the is different text in different branches for car
+
+    //this is our car from james branch
+
+    //moretext
+
+    //more text
+
 }

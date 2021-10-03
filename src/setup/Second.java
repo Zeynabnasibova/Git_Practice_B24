c752ede9efadad4b3414d22ca5b1d1b2261fe00b
zeynabnasibova <zeynabnasibova7@gmail.com>
2021-10-03 12:23:44 -0400

    updated second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,15 +3,15 @@ package setup;
 public class Second {
 
     public void m (){
-
+        System.out.println("first metod");
     }
     public void m(int a){
 
-
+        System.out.println("second metod");
     }
 
     public  void m(String s){
-
+        System.out.println("third metod");
 
     }
 }
